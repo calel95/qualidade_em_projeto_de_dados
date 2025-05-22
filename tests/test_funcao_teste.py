1cdbc0ad2dc853bb68a7411181bb80d95fd67242
Calel <borgescalel@gmail.com>
2025-05-21 23:34:41 -0300

    feat: adicionado test

--- tests/test_funcao_teste.py
+++ tests/test_funcao_teste.py
@@ -1,1 +1,6 @@
 from app.funcao import funcao_teste
+
+def test_funcao_teste():
+    # Testa se a função retorna a string correta
+    gabarito = "Hello, World!"
+    assert funcao_teste() == gabarito
